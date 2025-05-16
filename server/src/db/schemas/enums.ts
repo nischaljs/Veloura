237b393fab37ba16f13913b0082912960458e7a6
Nischal <nishchalb21@gmail.com>
2025-05-16 20:27:42 +0545

    removed drizzle and installed prisma

--- server/src/db/schemas/enums.ts
+++
@@ -1,9 +0,0 @@
-import { mysqlEnum } from "drizzle-orm/mysql-core";
-
-export const verificationStatus = mysqlEnum("verification_status", ["PENDING", "VERIFIED", "REJECTED"]);
-
-export const roles = mysqlEnum("role", ["CUSTOMER", "VENDOR", "ADMIN"]);
-
-export const paymentType = mysqlEnum("payment_type", ["BANK", "ESEWA", "KHALTI", "CONNECT_IPS"]);
-
-export const VendorOperationalType = mysqlEnum("operational_status",["OPEN","CLOSE","ON_BREAK"])
